575c9614751a938dabb3b9b9923198f4354298c0
od <Akkessun@gmail.com>
2025-10-27 15:58:29 +0900

    feat: project init

---
+++ src/main/java/com/odcloud/OdCloudApplication.java
@@ -0,0 +1,13 @@
+package com.odcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odcloud/OdCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
