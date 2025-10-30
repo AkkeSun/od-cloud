1869eee6b972177c25b33b647cafcd46057507e5
od <Akkessun@gmail.com>
2025-10-30 13:33:46 +0900

    modify(account): 임시토큰 생성시 관리자 승인 유무 체크하도록 수정

--- src/main/java/com/odcloud/application/service/issue_temp_token/IssueTempTokenService.java
+++ src/main/java/com/odcloud/application/service/issue_temp_token/IssueTempTokenService.java
@@ -1,9 +1,12 @@
 package com.odcloud.application.service.issue_temp_token;
 
+import static com.odcloud.infrastructure.exception.ErrorCode.Business_ADMIN_NOT_APPROVED;
+
 import com.odcloud.application.port.in.IssueTempTokenUseCase;
 import com.odcloud.application.port.in.command.IssueTempTokenCommand;
 import com.odcloud.application.port.out.AccountStoragePort;
 import com.odcloud.domain.model.Account;
+import com.odcloud.infrastructure.exception.CustomBusinessException;
 import com.odcloud.infrastructure.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,7 +22,11 @@ class IssueTempTokenService implements IssueTempTokenUseCase {
     public IssueTempTokenServiceResponse issue(IssueTempTokenCommand command) {
         Account account = accountStoragePort.findByUsernameAndPassword(
             command.username(), command.password());
-        String tempToken = jwtUtil.createTempToken(account.getUsername());
+        if (!account.isAdminApproved()) {
+            throw new CustomBusinessException(Business_ADMIN_NOT_APPROVED);
+        }
+
+        String tempToken = jwtUtil.createTempToken(account);
         return IssueTempTokenServiceResponse.of(tempToken);
     }
 }
