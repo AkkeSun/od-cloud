2a1fef98eac25e19c75f1f4a056a904df1209a75
od <Akkessun@gmail.com>
2025-11-03 13:28:00 +0900

    modify(common): 메일발송 로직 비동기로 변경

--- src/main/java/com/odcloud/adapter/out/mail/MailAdapter.java
+++ src/main/java/com/odcloud/adapter/out/mail/MailAdapter.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -20,6 +21,7 @@ class MailAdapter implements MailPort {
 
     private final JavaMailSender mailSender;
 
+    @Async
     @Override
     public void send(MailRequest request) {
         try {
