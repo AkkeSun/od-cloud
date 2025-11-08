6c97a1a2a701cd860658bace9cc630139f199362
od <akkessun@gmail.com>
2025-11-08 13:13:27 +0900

    modify(account): ApproveAccountController HttpMethod 변경

--- src/main/java/com/odcloud/adapter/in/approve_account/ApproveAccountController.java
+++ src/main/java/com/odcloud/adapter/in/approve_account/ApproveAccountController.java
@@ -4,8 +4,8 @@ import com.odcloud.application.port.in.ApproveAccountUseCase;
 import com.odcloud.application.service.approve_account.ApproveAccountServiceResponse;
 import com.odcloud.infrastructure.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +14,7 @@ class ApproveAccountController {
 
     private final ApproveAccountUseCase useCase;
 
-    @PutMapping("/accounts/{userId}/approval")
+    @PatchMapping("/accounts/{userId}/approval")
     ApiResponse<ApproveAccountResponse> confirm(@PathVariable Long userId) {
         ApproveAccountServiceResponse serviceResponse = useCase.approve(userId);
 
