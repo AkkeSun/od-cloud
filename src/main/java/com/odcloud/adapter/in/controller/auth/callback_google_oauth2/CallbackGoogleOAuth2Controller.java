f897a906170d26f35c0d01b725c5b2e62f768a86
od <akkessun@gmail.com>
2026-02-18 16:57:08 +0900

    rollback: 웹용 oauth2 callback api 수정 (롤백)

---
+++ src/main/java/com/odcloud/adapter/in/controller/auth/callback_google_oauth2/CallbackGoogleOAuth2Controller.java
@@ -0,0 +1,24 @@
+package com.odcloud.adapter.in.controller.auth.callback_google_oauth2;
+
+import com.odcloud.application.auth.port.in.CallbackGoogleOAuth2UseCase;
+import com.odcloud.application.auth.service.callback_google_oauth2.CallbackGoogleOAuth2ServiceResponse;
+import com.odcloud.infrastructure.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+class CallbackGoogleOAuth2Controller {
+
+    private final CallbackGoogleOAuth2UseCase useCase;
+
+    @GetMapping("/auth/google")
+    ApiResponse<CallbackGoogleOAuth2Response> callback(
+        @Valid CallbackGoogleOAuth2Request request
+    ) {
+        CallbackGoogleOAuth2ServiceResponse serviceResponse = useCase.callback(request.getCode());
+        return ApiResponse.ok(CallbackGoogleOAuth2Response.of(serviceResponse));
+    }
+}
--- src/main/java/com/odcloud/adapter/in/controller/auth/issue_token/IssueTokenController.java
+++ src/main/java/com/odcloud/adapter/in/controller/auth/issue_token/IssueTokenController.java
@@ -1,12 +1,9 @@
 package com.odcloud.adapter.in.controller.auth.issue_token;
 
-import com.odcloud.application.auth.port.in.CallbackGoogleOAuth2UseCase;
 import com.odcloud.application.auth.port.in.IssueTokenUseCase;
-import com.odcloud.application.auth.service.callback_google_oauth2.CallbackGoogleOAuth2ServiceResponse;
 import com.odcloud.application.auth.service.issue_token.IssueTokenServiceResponse;
 import com.odcloud.infrastructure.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,18 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 class IssueTokenController {
 
     private final IssueTokenUseCase useCase;
-    private final CallbackGoogleOAuth2UseCase googleOAuth2UseCase;
 
     @PostMapping("/auth")
     ApiResponse<IssueTokenResponse> issue(@RequestHeader String googleAuthorization) {
         IssueTokenServiceResponse serviceResponse = useCase.issue(googleAuthorization);
-        return ApiResponse.ok(IssueTokenResponse.of(serviceResponse));
-    }
-
-    @GetMapping("/auth/google")
-    ApiResponse<IssueTokenResponse> issueForWeb(String code) {
-        CallbackGoogleOAuth2ServiceResponse callback = googleOAuth2UseCase.callback(code);
-        IssueTokenServiceResponse serviceResponse = useCase.issue(callback.googleAccessToken());
         return ApiResponse.ok(IssueTokenResponse.of(serviceResponse));
     }
 }
