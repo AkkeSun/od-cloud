9c3e54f0b6d667c08ff7245d07f6b31c61046ff9
od <Akkessun@gmail.com>
2026-03-18 10:08:45 +0900

    chore: graceful 설정 추가

--- src/main/java/com/odcloud/infrastructure/config/SchedulingConfig.java
+++ src/main/java/com/odcloud/infrastructure/config/SchedulingConfig.java
@@ -1,12 +1,22 @@
 package com.odcloud.infrastructure.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Profile("!test")
 @Configuration
 @EnableScheduling
 public class SchedulingConfig {
 
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        scheduler.setAwaitTerminationSeconds(10);
+        return scheduler;
+    }
 }
